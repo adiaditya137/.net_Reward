ebf982ed5971458005aedca876e498572c5f4cbc
Adi <thombreadi+github@gmail.com>
2023-01-14 21:50:50 +0530

    Add project files.

---
+++ Reward/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Reward
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int day;
+            Console.WriteLine("Enter the week day number between(1-7)");
+
+            day = Convert.ToInt32(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+        }
+    }
+}
